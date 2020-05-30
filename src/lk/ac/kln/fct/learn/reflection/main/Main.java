d28136640f08422b55022310c9b58961fdc685eb
chamini19 <64278815+chamini19@users.noreply.github.com>
2020-05-31 00:52:34 +0530

    task-3

---
+++ src/lk/ac/kln/fct/learn/reflection/main/Main.java
@@ -0,0 +1,23 @@
+package lk.ac.kln.fct.learn.reflection.main;
+
+//import java.lang.reflect.InvocationTargetException;
+import lk.ac.kln.fct.learn.reflection.shapes.Rectangle;
+import java.lang.reflect.Field;
+
+public class Main {
+	public static void main(String[] args) {
+		Rectangle rectangle = null;				
+		try {
+			Class<?> clazz = Rectangle.class;
+			rectangle = (Rectangle) clazz.getConstructor().newInstance();
+			
+			Field field1 =clazz.getSuperclass().getDeclaredField("name");
+			field1.setAccessible(true);
+			field1.set(rectangle, "Rectangle");
+			
+		} catch (Exception e) {			
+			e.printStackTrace();
+		}
+		System.out.println("Shape name: "+ rectangle.getName());
+	}
+}
